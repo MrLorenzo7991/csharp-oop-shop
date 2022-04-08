413ee41f00a29e94df3665b3797f4556831eb47d
Lorenzo <lorenzo.miccio97@gmail.com>
2022-04-08 17:06:14 +0200

    Completati costruttori

--- csharp-oop-shop/Prodotto.cs
+++ csharp-oop-shop/Prodotto.cs
@@ -8,5 +8,39 @@ namespace csharp_oop_shop
 {
     internal class Prodotto
     {
+        //attributi
+        private string nome;    
+        private int codice;
+        private string descrizione;
+        private double prezzo;
+        private int iva;
+
+        //costruttore con descrizione
+        public Prodotto(string nome,string descrizione, int prezzo, int ivaApplicabile)
+        {
+            this.nome = nome;
+            this.prezzo = prezzo; 
+            this.descrizione = descrizione;
+            iva = ivaApplicabile;
+            
+            Random rnd = new Random();
+
+            codice = rnd.Next(1, 100000000);
+        }
+        //costruttore senza descizione
+        public Prodotto(string nome, int prezzo, int ivaApplicabile)
+        {
+            this.nome = nome;
+            this.prezzo = prezzo;
+            iva = ivaApplicabile;
+
+            Random rnd = new Random();
+
+            codice = rnd.Next(1, 100000000);
+        }
+
+        //Lettura attributi
+        
+        
     }
 }
